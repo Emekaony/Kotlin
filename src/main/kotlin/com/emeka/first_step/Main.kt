e1eef427162843e4daa87646816641bd450249eb
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2025-01-02 11:16:25 -0800

    Made some significant progress, gonna move to ktor now.

--- src/main/kotlin/com/emeka/first_step/Main.kt
+++ src/main/kotlin/com/emeka/first_step/Main.kt
@@ -1,7 +1,15 @@
 package main.kotlin.com.emeka.first_step
 
-import main.kotlin.com.emeka.first_step.functions.basics
-
 fun main( ) {
-    basics()
+    val nums = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    val filterOdd: (Int) -> Boolean = {it % 2 != 0}
+    val filterEven: (Int) -> Boolean = {it % 2 == 0}
+
+    filterList(nums, filterEven)
+    filterList(nums, filterOdd)
+}
+
+fun filterList(ll: List<Int>, predicate: (Int) -> Boolean) {
+    // this functional programming thing is madness, whatttttt!!!
+    ll.filter(predicate).forEach { println(it) }
 }
